ec5de4ef2a89f5086ee4cc4b9b3ebe76056bf631
bhuvanasritota <bhuvanasritota@gmail.com>
2026-07-13 21:07:35 +0530

    Day 6: CRUD APIs completed

--- backend/src/main/java/com/examsphere/controller/AuthController.java
+++ backend/src/main/java/com/examsphere/controller/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 import com.examsphere.entity.User;
 import com.examsphere.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
 @RestController
 public class AuthController {
@@ -27,5 +28,26 @@ private UserService userService;
     @PostMapping("/users")
 public User saveUser(@RequestBody User user) {
     return userService.saveUser(user);
+}
+@GetMapping("/users")
+public List<User> getAllUsers() {
+    return userService.getAllUsers();
+}
+
+@GetMapping("/users/{id}")
+public User getUserById(@PathVariable Long id) {
+    return userService.getUserById(id);
+}
+
+@PutMapping("/users/{id}")
+public User updateUser(@PathVariable Long id,
+                       @RequestBody User user) {
+    return userService.updateUser(id, user);
+}
+
+@DeleteMapping("/users/{id}")
+public String deleteUser(@PathVariable Long id) {
+    userService.deleteUser(id);
+    return "User Deleted Successfully";
 }
 }
